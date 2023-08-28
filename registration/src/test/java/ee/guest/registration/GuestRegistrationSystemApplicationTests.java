531b23ab5d2dfa87c97990d09bb080a1cbb3cfa1
Vadim <vafilo@ttu.ee>
2023-08-28 16:12:58 +0300

    initialize backend

---
+++ registration/src/test/java/ee/guest/registration/GuestRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.guest.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
