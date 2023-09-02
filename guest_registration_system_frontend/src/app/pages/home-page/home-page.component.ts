037b3f90cbf47a26854a8e8df1e215a2e7c6f1cb
Vadim <vafilo@ttu.ee>
2023-09-02 12:52:09 +0300

    display all events at home page

--- guest_registration_system_frontend/src/app/pages/home-page/home-page.component.ts
+++ guest_registration_system_frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ApiService} from "../../services/api.service";
+import {IEvent} from "../../models/IEvent";
 
 @Component({
   selector: 'app-home-page',
@@ -7,7 +9,33 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
-  constructor() {
+  public previousEvents: IEvent[] = []
+  public futureEvents: IEvent[] = []
+
+  constructor(private api: ApiService) {
+    this.api.sendGetRequest("/v1/user").subscribe(
+      response => {
+        let events: IEvent[] = response as IEvent[];
+        const currentDate = new Date();
+        for (const event of events) {
+          const eventDate = new Date(event.date);
+
+          if (eventDate < currentDate) {
+            this.previousEvents.push(event);
+          } else {
+            this.futureEvents.push(event);
+          }
+        }
+
+        this.previousEvents.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+        this.futureEvents.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+    }, error => {
+      console.log(error)
+    })
   }
 
+  // TODO if organizer remove event else leave from event
+  remove() {
+
+  }
 }
